ba6bafcf3ac35689c96259e6e207c144ab61b68d
rsanzfloridauni <rsanz@florida-uni.es>
2025-02-09 23:11:41 +0100

    commit 1

---
+++ src/calculadora.php
@@ -0,0 +1,25 @@
+<?php
+namespace App;
+class Calculadora
+{
+    public function suma($a, $b)
+    {
+        return $a + $b;
+    }
+    public function resta($a, $b)
+    {
+        return $a - $b;
+    }
+    public function multiplicacion($a, $b)
+    {
+        return $a * $b;
+    }
+    public function division($a, $b)
+    {
+        return $a / $b;
+    }
+    public function raiz($a)
+    {
+        return sqrt($a);
+    }
+}
